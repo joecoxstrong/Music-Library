5966a69dfa43045c2bc0678da151e334a425f52b
joecoxstrong <joecoxstrong@gmail.com>
2022-04-07 13:03:14 -0400

    Removed music_library app + added logic to songs.views to allow GET,POST, PUT, DELETE + added urls for both songs and music_library_project

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music_library',
     'songs',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'music_library_project.urls'
@@ -115,6 +116,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL =True
 
 try:
     from music_library_project.local_settings import *
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import imp
+from urllib import response
+from rest_framework.decorators import api_view
+from .serializer import SongSerializer
+from songs.models import Song
+from rest_framework import status
+from .models import Song
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def songs_list(request):
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+  
+@api_view(['GET','PUT','DELETE'])     
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk = pk)
+    if request.method == 'GET': 
+        serializer = SongSerializer(song);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)   
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
